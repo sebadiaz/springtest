6749ce3335e38a612979e0aa57b665c816e90057
Sebastien Diaz <sebastien.diaz@misys.com>
2018-03-15 13:19:39 +0100

    rte

--- olocrat/src/main/java/org/olo/newton/NewtonFactory.java
+++ olocrat/src/main/java/org/olo/newton/NewtonFactory.java
@@ -1,5 +1,13 @@
 package org.olo.newton;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class NewtonFactory {
 
+	@Bean
+	public NewtonBean get() {
+		return new NewtonBean();
+	}
 }

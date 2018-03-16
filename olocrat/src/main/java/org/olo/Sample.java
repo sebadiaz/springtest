fd6bca0f8d33bf25a1feb55c6c3c7a4c3e37d8bf
Sebastien Diaz <sebastien.diaz@misys.com>
2018-03-16 09:57:50 +0100

    rrr

--- olocrat/src/main/java/org/olo/Sample.java
+++ olocrat/src/main/java/org/olo/Sample.java
@@ -1,19 +1,23 @@
 package org.olo;
+import org.olo.newton.NewtonFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @EnableAutoConfiguration
+@SpringBootApplication
 public class Sample {
 
 	/**
 	 * http://www.irem.univ-mrs.fr/IMG/pdf/methode_de_newton.pdf
 	 * @return
 	 */
-		@RequestMapping("/")
+
+		//@RequestMapping("/")
 			String home() {
-						return "Hello World!";
+					return "Hello";
 							}
 
 			public static void main(String[] args) throws Exception {
--- olocrat/src/test/java/olo/SampleTest.java
+++ olocrat/src/test/java/olo/SampleTest.java
@@ -8,7 +8,7 @@ public class SampleTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		assertTrue(true);
 	}
 
 }

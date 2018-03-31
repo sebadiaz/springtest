ef26c1e75b7e74934fee4ea0e993e813dbf19f85
Sebastien DIAZ <sebastien.diaz@gmail.com>
2018-03-31 22:30:56 +0200

    uyu

--- olocrat/src/main/java/org/olo/rest/Service.java
+++ olocrat/src/main/java/org/olo/rest/Service.java
@@ -3,6 +3,7 @@ package org.olo.rest;
 import org.olo.newton.NewtonFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
